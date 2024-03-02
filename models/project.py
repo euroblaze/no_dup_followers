11b647266e1b81946a80616ca9b495a663422207
ngkiencuong <85164180+ngkiencuong@users.noreply.github.com>
2024-03-03 03:20:55 +0700

    Fix the code

--- models/project.py
+++ models/project.py
@@ -1,4 +1,5 @@
 from odoo import models, api, fields
+from odoo.tools.misc import clean_context
 
 
 class ProjectProject(models.Model):
@@ -6,12 +7,9 @@ class ProjectProject(models.Model):
 
     def copy(self, default=None):
         no_dup_followers = self.env['ir.config_parameter'].sudo().get_param('no_dup_followers.no_duplicating_followers_project', False)
-        if not no_dup_followers:
-            return super(ProjectProject, self).copy(default)
-        res = super(ProjectProject, self).copy(default)
-        for rec in res:
-            self.env['mail.followers'].search([('res_model', '=', 'project.project'), ('res_id', '=', rec.id)]).unlink()
-        return res
+        if no_dup_followers:
+            self = self.with_context(no_dup_followers=True)
+        return super(ProjectProject, self).copy(default)
 
 
 class ProjectTask(models.Model):
@@ -19,9 +17,7 @@ class ProjectTask(models.Model):
 
     def copy(self, default=None):
         no_dup_followers = self.env['ir.config_parameter'].sudo().get_param('no_dup_followers.no_duplicating_followers_project', False)
-        if not no_dup_followers:
-            return super(ProjectTask, self).copy(default)
-        res = super(ProjectTask, self).copy(default)
-        for rec in res:
-            self.env['mail.followers'].search([('res_model', '=', 'project.task'), ('res_id', '=', rec.id)]).unlink()
-        return res
+        if no_dup_followers:
+            self = self.with_context(no_dup_followers=True)
+        return super(ProjectTask, self).copy(default)
+
